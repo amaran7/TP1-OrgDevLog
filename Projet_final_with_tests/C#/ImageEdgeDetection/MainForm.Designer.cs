e2605d452ea29215b1ef6974f2c266170afee503
amaran7 <elsio.coelho@gmail.com>
2016-10-07 06:16:39 +0200

    TP2 prêt à démarrer
    Projet_final_with_tests

---
+++ Projet_final_with_tests/C#/ImageEdgeDetection/MainForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ImageEdgeDetection
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            this.btnOpenOriginal = new System.Windows.Forms.Button();
+            this.btnSaveNewImage = new System.Windows.Forms.Button();
+            this.cmbEdgeDetection = new System.Windows.Forms.ComboBox();
+            this.cmbFilterList = new System.Windows.Forms.ComboBox();
+            this.P = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPreview
+            // 
+            this.picPreview.BackColor = System.Drawing.Color.Silver;
+            this.picPreview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picPreview.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.picPreview.Location = new System.Drawing.Point(12, 12);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(634, 600);
+            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPreview.TabIndex = 13;
+            this.picPreview.TabStop = false;
+            // 
+            // btnOpenOriginal
+            // 
+            this.btnOpenOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenOriginal.Location = new System.Drawing.Point(13, 697);
+            this.btnOpenOriginal.Name = "btnOpenOriginal";
+            this.btnOpenOriginal.Size = new System.Drawing.Size(119, 37);
+            this.btnOpenOriginal.TabIndex = 15;
+            this.btnOpenOriginal.Text = "Load Image";
+            this.btnOpenOriginal.UseVisualStyleBackColor = true;
+            this.btnOpenOriginal.Click += new System.EventHandler(this.btnOpenOriginal_Click);
+            // 
+            // btnSaveNewImage
+            // 
+            this.btnSaveNewImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveNewImage.Location = new System.Drawing.Point(544, 697);
+            this.btnSaveNewImage.Name = "btnSaveNewImage";
+            this.btnSaveNewImage.Size = new System.Drawing.Size(102, 33);
+            this.btnSaveNewImage.TabIndex = 16;
+            this.btnSaveNewImage.Text = "Save as...";
+            this.btnSaveNewImage.UseVisualStyleBackColor = true;
+            this.btnSaveNewImage.Click += new System.EventHandler(this.btnSaveNewImage_Click);
+            // 
+            // cmbEdgeDetection
+            // 
+            this.cmbEdgeDetection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEdgeDetection.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbEdgeDetection.FormattingEnabled = true;
+            this.cmbEdgeDetection.Items.AddRange(new object[] {
+            "None",
+            "Laplacian 3x3",
+            "Laplacian 3x3 Grayscale",
+            "Laplacian 5x5",
+            "Laplacian 5x5 Grayscale",
+            "Laplacian of Gaussian",
+            "Laplacian 3x3 of Gaussian 3x3",
+            "Laplacian 3x3 of Gaussian 5x5 - 1",
+            "Laplacian 3x3 of Gaussian 5x5 - 2",
+            "Laplacian 5x5 of Gaussian 3x3",
+            "Laplacian 5x5 of Gaussian 5x5 - 1",
+            "Laplacian 5x5 of Gaussian 5x5 - 2",
+            "Sobel 3x3",
+            "Sobel 3x3 Grayscale",
+            "Prewitt",
+            "Prewitt Grayscale",
+            "Kirsch",
+            "Kirsch Grayscale"});
+            this.cmbEdgeDetection.Location = new System.Drawing.Point(334, 650);
+            this.cmbEdgeDetection.Name = "cmbEdgeDetection";
+            this.cmbEdgeDetection.Size = new System.Drawing.Size(312, 32);
+            this.cmbEdgeDetection.TabIndex = 20;
+            this.cmbEdgeDetection.SelectedIndexChanged += new System.EventHandler(this.NeighbourCountValueChangedEventHandler);
+            // 
+            // cmbFilterList
+            // 
+            this.cmbFilterList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFilterList.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbFilterList.FormattingEnabled = true;
+            this.cmbFilterList.Items.AddRange(new object[] {
+            "None",
+            "Night Filter",
+            "Hell Filter",
+            "Miami Filter",
+            "Zen Filter",
+            "Black and White",
+            "Swap Filter",
+            "Crazy Filter",
+            "Mega Filter Green",
+            "Mega Filter Orange",
+            "Mega Filter Pink",
+            "Mega Filter Custom",
+            "Rainbow Filter"});
+            this.cmbFilterList.Location = new System.Drawing.Point(12, 650);
+            this.cmbFilterList.Name = "cmbFilterList";
+            this.cmbFilterList.Size = new System.Drawing.Size(304, 32);
+            this.cmbFilterList.TabIndex = 21;
+            this.cmbFilterList.SelectedIndexChanged += new System.EventHandler(this.cmbFilterList_SelectedIndexChanged);
+            // 
+            // P
+            // 
+            this.P.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.P.Location = new System.Drawing.Point(9, 622);
+            this.P.Name = "P";
+            this.P.Size = new System.Drawing.Size(100, 23);
+            this.P.TabIndex = 0;
+            this.P.Text = "Filters";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(330, 620);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 29);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Edge detection";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(658, 742);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.P);
+            this.Controls.Add(this.cmbFilterList);
+            this.Controls.Add(this.cmbEdgeDetection);
+            this.Controls.Add(this.btnSaveNewImage);
+            this.Controls.Add(this.btnOpenOriginal);
+            this.Controls.Add(this.picPreview);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TP2";
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPreview;
+        private System.Windows.Forms.Button btnOpenOriginal;
+        private System.Windows.Forms.Button btnSaveNewImage;
+        private System.Windows.Forms.ComboBox cmbEdgeDetection;
+        private System.Windows.Forms.ComboBox cmbFilterList;
+        private System.Windows.Forms.Label P;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Projet_final_with_tests/C#/ImageEdgeDetection/MainForm.cs
@@ -0,0 +1,237 @@
+﻿/*
+ * The Following Code was developed by Dewald Esterhuizen
+ * View Documentation at: http://softwarebydefault.com
+ * Licensed under Ms-PL 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ImageEdgeDetection
+{
+    public partial class MainForm : Form
+    {
+        private Bitmap originalBitmap = null;
+        private Bitmap previewBitmap = null;
+        private Bitmap resultBitmap = null;
+        private Boolean isImageFiltered;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+
+            cmbEdgeDetection.SelectedIndex = 0;
+            cmbFilterList.SelectedIndex = 0;
+            cmbEdgeDetection.Enabled = false;
+            cmbFilterList.Enabled = false;
+            isImageFiltered = false;
+        }
+
+        private void btnOpenOriginal_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file.";
+            ofd.Filter = "PNG Images (*.png)|*.png|JPEG Images (*.jpg)|*.jpg";
+            ofd.Filter += "|BMP Images (*.bmp)|*.bmp";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(ofd.FileName);
+                originalBitmap = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+                streamReader.Close();
+
+                previewBitmap = originalBitmap.CopyToSquareCanvas(picPreview.Width);
+                picPreview.Image = previewBitmap;
+
+                ApplyFilter(true);
+                cmbFilterList.Enabled = true;
+                cmbEdgeDetection.Enabled = true;
+            }
+        }
+
+        private void btnSaveNewImage_Click(object sender, EventArgs e)
+        {
+            ApplyFilter(false);
+
+            if (resultBitmap != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Specify a file name and file path";
+                sfd.Filter = "PNG Images (*.png)|*.png|JPEG Images (*.jpg)|*.jpg";
+                sfd.Filter += "|BMP Images (*.bmp)|*.bmp";
+
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string fileExtension = Path.GetExtension(sfd.FileName).ToUpper();
+                    ImageFormat imgFormat = ImageFormat.Png;
+
+                    if (fileExtension == "BMP")
+                    {
+                        imgFormat = ImageFormat.Bmp;
+                    }
+                    else if (fileExtension == "JPG")
+                    {
+                        imgFormat = ImageFormat.Jpeg;
+                    }
+
+                    StreamWriter streamWriter = new StreamWriter(sfd.FileName, false);
+                    resultBitmap.Save(streamWriter.BaseStream, imgFormat);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+
+                    resultBitmap = null;
+                }
+            }
+        }
+
+        private void ApplyFilter(bool preview)
+        {
+            if (previewBitmap == null || cmbEdgeDetection.SelectedIndex == -1)
+            {
+                return;
+            }
+
+            Bitmap selectedSource = null;
+            Bitmap bitmapResult = null;
+
+            if (preview == true)
+            {
+                selectedSource = previewBitmap;
+            }
+            else
+            {
+                if (isImageFiltered)
+                {
+                    selectedSource = previewBitmap;
+                }
+                else
+                {
+                    selectedSource = originalBitmap;
+                }
+                
+            }
+
+            if (selectedSource != null)
+            {
+                if (cmbEdgeDetection.SelectedItem.ToString() == "None")
+                {
+                    bitmapResult = selectedSource;
+                    cmbFilterList.Enabled = true;
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 3x3")
+                {
+                    bitmapResult = selectedSource.Laplacian3x3Filter(false);
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 3x3 Grayscale")
+                {
+                    bitmapResult = selectedSource.Laplacian3x3Filter(true);
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 5x5")
+                {
+                    bitmapResult = selectedSource.Laplacian5x5Filter(false);
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 5x5 Grayscale")
+                {
+                    bitmapResult = selectedSource.Laplacian5x5Filter(true);
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian of Gaussian")
+                {
+                    bitmapResult = selectedSource.LaplacianOfGaussianFilter();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 3x3 of Gaussian 3x3")
+                {
+                    bitmapResult = selectedSource.Laplacian3x3OfGaussian3x3Filter();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 3x3 of Gaussian 5x5 - 1")
+                {
+                    bitmapResult = selectedSource.Laplacian3x3OfGaussian5x5Filter1();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 3x3 of Gaussian 5x5 - 2")
+                {
+                    bitmapResult = selectedSource.Laplacian3x3OfGaussian5x5Filter2();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 5x5 of Gaussian 3x3")
+                {
+                    bitmapResult = selectedSource.Laplacian5x5OfGaussian3x3Filter();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 5x5 of Gaussian 5x5 - 1")
+                {
+                    bitmapResult = selectedSource.Laplacian5x5OfGaussian5x5Filter1();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Laplacian 5x5 of Gaussian 5x5 - 2")
+                {
+                    bitmapResult = selectedSource.Laplacian5x5OfGaussian5x5Filter2();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Sobel 3x3")
+                {
+                    bitmapResult = selectedSource.Sobel3x3Filter(false);
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Sobel 3x3 Grayscale")
+                {
+                    bitmapResult = selectedSource.Sobel3x3Filter();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Prewitt")
+                {
+                    bitmapResult = selectedSource.PrewittFilter(false);
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Prewitt Grayscale")
+                {
+                    bitmapResult = selectedSource.PrewittFilter();
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Kirsch")
+                {
+                    bitmapResult = selectedSource.KirschFilter(false);
+                }
+                else if (cmbEdgeDetection.SelectedItem.ToString() == "Kirsch Grayscale")
+                {
+                    bitmapResult = selectedSource.KirschFilter();
+                }
+            }
+
+            if (bitmapResult != null)
+            {
+                if (preview == true)
+                {
+                    picPreview.Image = bitmapResult;
+                }
+                else
+                {
+                    resultBitmap = bitmapResult;
+                }
+            }
+        }
+
+        private void NeighbourCountValueChangedEventHandler(object sender, EventArgs e)
+        {
+            cmbFilterList.Enabled = false;
+            ApplyFilter(true);
+            
+        }
+
+        private void cmbFilterList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbFilterList.SelectedItem.ToString() == "None")
+            {
+                previewBitmap = originalBitmap;
+                picPreview.Image = originalBitmap;
+                isImageFiltered = false;
+            }
+            else
+            {
+                previewBitmap = ImageFilters.ApplyImageFilter(originalBitmap, cmbFilterList.SelectedItem.ToString());
+                picPreview.Image = previewBitmap;
+                isImageFiltered = true;
+
+            }
+            
+        }
+
+    }
+}
